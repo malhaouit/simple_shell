e90ecc1c5e43ba9665fc99608f48fee6b90ed71b
Twaza-j <atwazagye@gmail.com>
2023-10-18 06:45:22 -0700

    Ends the custom shell

---
+++ handle_exit.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * exit_shell - Ends the custom shell
+ * @buffer: Buffer containing the command
+ *
+ * Return: 1 if buffer contains "exit" command, or -1 if not
+ */
+int exit_shell(char *buffer)
+{
+	if (_strcmp(buffer, "exit\n") == 0)
+	{
+		free(buffer);
+		return (-1);
+	}
+	return (1);
+}
